42525a722f025426f517e724e3012936dc5297da
quitenormal <daojira11@gmail.com>
2022-06-23 20:16:57 +0700

    Testing

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yjirapin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 20:13:03 by yjirapin          #+#    #+#             */
-/*   Updated: 2022/06/23 19:45:50 by yjirapin         ###   ########.fr       */
+/*   Updated: 2022/06/23 20:16:37 by yjirapin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,25 @@ and mainman as input and returns a string
 using malloc. (+1 is for the ending /0)
 Note that we check if memory allocation
 is successful.
-2) Starting out with read_bytes = 1 (just so that
+2) Starting out with bytes_read = 1 (just so that
 it will enter the read while loop at least once)
 3) ft_strchr is used to check if there a newline
 in mainman. If there is not a newline in mainman or
-if read_bytes is not 0 then we keep on going.
-4) If there is no newline in mainman and the read_bytes isn't
+if bytes_read is not 0 then we keep on going.
+4) If there is no newline in mainman and the bytes_read isn't
 zero, then the file is read into the variable buff.
 5) Read quits if an error is encountered.
 In other words, read returns -1 if there is an error in which case
 	the buff variable is free and the execution
 	is returned to the calling function.
-6) After a successful read, the read_bytes is changed to the
+6) After a successful read, the bytes_read is changed to the
 number of bytes read into buff.
 7) \0 is added to the end of the buff variable with
-buff[read_bytes] = '\0';
+buff[bytes_read] = '\0';
 8) Then mainman gets appended with the string in buff
 using the str_join function.
 9) Step 3 - 8 is repeated until mainman contains a newline
-or until read_bytes = 0 (which means that we're at the
+or until bytes_read = 0 (which means that we're at the
 end of the file).
 10) memory for buff variable is freed and mainman is returned.
 Note that mainman is a static variable so it
@@ -47,21 +47,21 @@ doesn't get reset.
 char	*ft_read4myman(int fd, char *mainman)
 {
 	char	*buff;
-	int		read_bytes;
+	int		bytes_read;
 
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	read_bytes = 1;
-	while (!check4nl(mainman) && read_bytes != 0)
+	bytes_read = 1;
+	while (!check4nl(mainman) && bytes_read != 0)
 	{
-		read_bytes = read(fd, buff, BUFFER_SIZE);
-		if (read_bytes == -1)
+		bytes_read = read(fd, buff, BUFFER_SIZE);
+		if (bytes_read == -1)
 		{
 			free(buff);
 			return (NULL);
 		}
-		buff[read_bytes] = '\0';
+		buff[bytes_read] = '\0';
 		mainman = ft_strjoin(mainman, buff);
 	}
 	free(buff);
@@ -111,9 +111,11 @@ int	main(void) {
 	int		fd1;
 	int		fd2;
 	int		fd3;
+	int		fd4;
 	fd1 = open("testme1.txt", O_RDONLY);
 	fd2 = open("testme2.txt", O_RDONLY);
 	fd3 = open("testme3.txt", O_RDONLY);
+	fd4 = open("testme4.txt", O_RDONLY);
 	i = 1;
 	while (i < 7){
 		line = get_next_line(fd1);
